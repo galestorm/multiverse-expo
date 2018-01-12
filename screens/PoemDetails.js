5db1878eea055a6d8c17e6b710d099d51263ccee
Gale Harrington <gale.harrington19@gmail.com>
2018-01-12 11:33:47 -0800

    Destructure poem in poem details

--- screens/PoemDetails.js
+++ screens/PoemDetails.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class PoemDetail extends Component {
+  constructor(props) {
+    super(props);
+
+    const { title, author, lines, source } = this.props.navigation.state.params.poem;
+    this.poem = { title, author, lines, source };
+  }
   render() {
     return (
       <View style={styles.detailContainer}>
-        <Text>{this.props.navigation.state.params.poem.title}</Text>
+        <Text>{this.poem.title}</Text>
       </View>
     );
   }
